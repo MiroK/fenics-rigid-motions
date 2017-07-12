25faa974c850561a799e87b926cc423523328956
mirok <miroslav.kuchta@gmail.com>
2017-07-12 17:28:49 +0200

    update

--- lagrange_mixed.py
+++ lagrange_mixed.py
@@ -35,6 +35,7 @@ def lagrange_mixed(lmbda, mu, f, h, mesh, Z=None):
     # For cube
     V = VectorFunctionSpace(mesh, 'CG', 2)
     Q = FunctionSpace(mesh, 'CG', 1)
+
     u, v = TrialFunction(V), TestFunction(V)
     p, q = TrialFunction(Q), TestFunction(Q)
 
@@ -43,8 +44,8 @@ def lagrange_mixed(lmbda, mu, f, h, mesh, Z=None):
     # Stress
     gdim = mesh.geometry().dim()
     sigma = lambda u: 2*mu*epsilon(u) + lmbda*tr(epsilon(u))*Identity(gdim)
-
     a = 2*mu*inner(sym(grad(u)), sym(grad(v)))*dx
+
     A = assemble(a)
 
     c = inner(div(v), p)*dx
@@ -80,7 +81,7 @@ def lagrange_mixed(lmbda, mu, f, h, mesh, Z=None):
     IX = rigid_motions.identity_matrix(X)
     BB = block_mat([[AMG(IV), 0,              0], 
                     [0,       AMG(IQ),        0],
-                    [0,       0,        AMG(IX)]])
+                    [0,       0,            IX]])
 
     # Solve, using random initial guess
     x0 = AA.create_vec()
@@ -90,19 +91,22 @@ def lagrange_mixed(lmbda, mu, f, h, mesh, Z=None):
                    show=2, relativeconv=True)
 
     x = AAinv*bb
-    
-    # Functions from coefficients
-    # uh = Function(V, x[0])     # Displacement
-    # ph = Function(Q, x[1])     # Solid pressure
-    # nuh = Zh.rigid_motion(x[2])  # Function in V
+
+    # # Functions from coefficients
+    # # uh = Function(V, x[0])     # Displacement
+    # # ph = Function(Q, x[1])     # Solid pressure
+    # # nuh = Zh.rigid_motion(x[2])  # Function in V
 
     niters = len(AAinv.residuals) - 1
     assert niters < 120
 
     P = rigid_motions.Projector(Zh)
-    P*x[0]  # to get orthogonality
+
+    P*x0[0]  # to get orthogonality
+
     if MPI.rank(mesh.mpi_comm()) == 0:
         print '\033[1;37;31m%s\033[0m' % ('Orthogonality %g' % max(P.alphas))
+        pass
 
     return V.dim() + Q.dim() + 6, niters
 
@@ -111,7 +115,6 @@ def test_lagrange_mixed():
     '''Number of iterations should not blow up'''
     from numpy import savetxt, array, c_
 
-    lmbda = Constant(1)
     mu = Constant(1)
     f = Expression(('A*sin(2*x[0])', 'A*cos(3*(x[0]+x[1]+x[2]))', 'A*sin(x[2])'),
                     degree=3, A=0.01)
@@ -127,7 +130,7 @@ def test_lagrange_mixed():
     lmbdas = 1E12, 1E8, 1E4, 1E0
     for lmbda in lmbdas:
         meshes = (BoxMesh(Point(1, 1, 1), Point(2, 1.5, 1.25), N, N, N) for N in Ns)
-        converged = lagrange_mixed(lmbda, mu, f, h, meshes)
+        converged = lagrange_mixed(Constant(lmbda), mu, f, h, meshes)
         assert all(converged)
 
         if len(data) == 0:
